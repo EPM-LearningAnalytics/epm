792ac8d6a62485b8d2abdf09615bb77c7dbcf6a9
wenjinzh <wjzh@uw.edu>
2021-12-09 05:47:45 -0800

    test_graph updated

--- epm/tests/test_graph_fun.py
+++ epm/tests/test_graph_fun.py
@@ -4,4 +4,51 @@ Tests for functions in graph_fun
 
 import unittest
 
+from epm.graph.graph_data import *
 from epm.graph.graph_fun import *
+
+class TestPlotMid(unittest.TestCase):
+    """
+    """
+    @staticmethod
+    def test_smoke(self):
+        """
+        Simple smoke test to make sure function runs.
+        """
+        plot_mid(mid_avg().head())
+
+    def test_input(self):
+        """
+        Simple smoke test to make sure function runs.
+        """
+        plot_mid(1)
+
+    def test_input_structure(self):
+        """
+        Simple smoke test to make sure function runs.
+        """
+        plot_mid(mid_avg().drop(['Student Id']))
+
+class TestPlotFinal(unittest.TestCase):
+    """
+    """
+    @staticmethod
+    def test_smoke(self):
+        """
+        Simple smoke test to make sure function runs.
+        """
+        data=final_mid(final_step_1())
+        plot_final(data.head())
+
+    def test_final_input(self):
+        """
+        Simple smoke test to make sure function runs.
+        """
+        plot_final(1)
+
+    def test_final_input_structure(self):
+        """
+        Simple smoke test to make sure function runs.
+        """
+        data=final_avg(final_step_1())
+        plot_final(data.drop(['Student ID']))
