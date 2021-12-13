e7de9325bf17ec557a3456b715f51aa3b0e3eaa6
mereluo <meredithluo710@gmail.com>
2021-12-12 21:11:08 -0800

    tests for graph_fun

--- epm/tests/test_graph_fun.py
+++ epm/tests/test_graph_fun.py
@@ -19,10 +19,10 @@ class TestPlotLog(unittest.TestCase):
         df = session_agg()
         df_avg = session_avg(df)
         student = 1
-        selected_activity = sorted( df['activity'].unique() )
+        activity = sorted( df['activity'].unique() )
         option = 'mouse_click_left'
 
-        p = plot_log(df_avg, student, selected_activity, option, type='average')
+        p = plot_log(df_avg, student, activity, option, type='average')
 
         self.assertEqual(str(type(p)), "<class 'altair.vegalite.v4.api.Chart'>")
 
@@ -32,11 +32,40 @@ class TestPlotLog(unittest.TestCase):
         Smoke test to make sure the student log graph
         has the right type
         """
+        # read datasets and prepare arguments
         df = session_agg()
         student = 1
-        selected_activity = sorted( df['activity'].unique() )
+        activity = sorted( df['activity'].unique() )
         option = 'mouse_click_left'
 
-        p = plot_log(df, student, selected_activity, option, type='student')
+        p = plot_log(df, student, activity, option, type='student')
 
         self.assertEqual(str(type(p)), "<class 'altair.vegalite.v4.api.Chart'>")
+    
+    def test_plot_type(self):
+        """
+        Edge test to make sure the function throws a ValueError
+        when the input type is neither 'average' nor 'student'
+        """
+        with self.assertRaises(ValueError):
+            df = session_agg()
+            student = 1
+            activity = sorted( df['activity'].unique() )
+            option = 'mouse_click_left'
+            type='individual'
+
+            plot_log(df, student, activity, option, type)
+    
+    def test_data_type(self):
+        """
+        Edge test to make sure the function throws a ValueError
+        when the input data is not the desired one
+        """
+        with self.assertRaises(ValueError):
+            df = session_agg()
+            student = 1
+            activity = sorted( df['activity'].unique())
+            option = 'mouse_click_left'
+
+            plot_log(df, student, activity, option, type='average')
+            
