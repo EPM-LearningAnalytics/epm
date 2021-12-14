b0c5ea413fea7bfe6c2a797dcd02cd798d87ffcc
kimyoungwon <42983671+kimyoungwon@users.noreply.github.com>
2021-12-14 14:39:05 -0800

    updated data_prep

--- epm/data_prep/grades_prep.py
+++ epm/data_prep/grades_prep.py
@@ -4,103 +4,100 @@
 This module preprocess the grades files
 """
 
-
-import os
-import glob
 import pandas as pd
-import scipy.stats as stats
 import numpy as np
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 
-
-def read_grades(dir1 = '',
-                dir2 = ''):
+def read_grades(dir1='EPM_Project/EPM_dataset/Data/intermediate_grades.xlsx',
+                dir2='EPM_Project/EPM_dataset/Data/final_grades.xlsx'):
     """
     Read grades.xlsx files
-
     Parameters
     ----------
     dir1: intermidiate grades path
     dir2: final grades path
     """
-    # get intermediate grades
+    # Get intermediate grades
     mid_grades = pd.read_excel(dir1)
-    
-    # get final grades
+    # Get final grades
     xl_file = pd.ExcelFile(dir2)
     sheet_name = xl_file.sheet_names
     final_1st = pd.read_excel(dir2, sheet_name=sheet_name[0])
     final_2nd = pd.read_excel(dir2, sheet_name=sheet_name[1])
-    
     return mid_grades, final_1st, final_2nd
 
 
-def final_manipulation(final_1st,final_2nd):
+def final_manipulation(final_1st, final_2nd):
     """
     This function merge two final grades into one single file
-
     Parameters
     ----------
     final_1st: first final grades dataframe
     final_2nd: second final grades dataframe
-
     Return
     ----------
     A dataframe of one final grades
-
     """
-    # students who took exams twice
+    # Error meassages
+    if not isinstance(final_1st, pd.DataFrame) is True:
+        raise ValueError("'final_1st' should be should be a panda dataframe.")
+    if not isinstance(final_2nd, pd.DataFrame) is True:
+        raise ValueError("'final_2nd' should be should be a panda dataframe.")
+    # Students who took exams twice
     twotimer = list(set(final_1st['Student ID']).intersection(set(final_2nd['Student ID'])))
     # 1st test takers without taking the second exam
-    final_2nd_unique=final_2nd[-final_2nd['Student ID'].isin(twotimer)]
-    # students who took final exams (selected twotimers' first exam score)
+    final_2nd_unique = final_2nd[-final_2nd['Student ID'].isin(twotimer)]
+    # Students who took final exams (selected twotimers' first exam score)
     final = final_1st.append(final_2nd_unique).sort_values(by=['Student ID'])
-
     final_data = {'ID': final['Student ID']}
-    
     # aggregate grades of each session
     final_data['FIN1'] = (final['ES 1.1 \n(2 points)'] + final['ES 1.2 \n(3 points)']) / 5 * 100
     final_data['FIN2'] = (final['ES 2.1\n(2 points)'] + final['ES 2.2\n(3 points)']) / 5 * 100
-    final_data['FIN3'] = (final['ES 3.1\n(1 points)'] + final['ES 3.2\n(2 points)'] + final['ES 3.3\n(2 points)'] + final['ES 3.4\n(2 points)'] + final['ES 3.5\n(3 points)']) / 10 * 100
+    final_data['FIN3'] = (final['ES 3.1\n(1 points)'] + final['ES 3.2\n(2 points)'] +
+                          final['ES 3.3\n(2 points)'] + final['ES 3.4\n(2 points)'] +
+                          final['ES 3.5\n(3 points)']) / 10 * 100
     final_data['FIN4'] = (final['ES 4.1\n(15 points)'] + final['ES 4.2\n(10 points)']) / 25 * 100
-    final_data['FIN5'] = (final['ES 5.1\n(2 points)'] + final['ES 5.2\n(10 points)'] + final['ES 5.3\n(3 points)']) / 15 * 100
+    final_data['FIN5'] = (final['ES 5.1\n(2 points)'] + final['ES 5.2\n(10 points)'] +
+                          final['ES 5.3\n(3 points)']) / 15 * 100
     final_data['FIN6'] = (final['ES 6.1\n(25 points)'] + final['ES 6.2\n(15 points)']) / 40 * 100
     final_100 = pd.DataFrame(data=final_data)
-    final = ['FIN1','FIN2','FIN3','FIN4','FIN5','FIN6']
+    final = ['FIN1', 'FIN2', 'FIN3', 'FIN4', 'FIN5', 'FIN6']
     final_100['FIN1'] = final_100['FIN1'].fillna(0)
-    final_100["final_score"] = final_100[final].sum(axis = 1)/6
-    final_100
-
+    final_100["final_score"] = final_100[final].sum(axis=1)/6
     return final_100
 
 
 def rebase_mid(mid_grades):
     """
     Rescale mid scores to scores out of 100
-    
+
     Parameters
     ----------
-    final_1st: first final grades dataframe
-    final_2nd: second final grades dataframe
+    mid_grades: first final grades dataframe
 
     Return
     ----------
-    A dataframe of one final grades
+    A dataframe of converted mid scores into 100 score bases
     """
+    # Error meassage
+    if not isinstance(mid_grades, pd.DataFrame) is True:
+        raise ValueError("'final_1st' should be should be a panda dataframe.")
+    # Rebase score on 100
     mid_100 = mid_grades
     for i, col_name in enumerate(mid_grades.columns):
-        if i == 0: continue
+        if i == 0:
+            continue
         max_score = mid_grades[[col_name]].max()
         mid_100[[col_name]] = round(mid_grades[[col_name]] / max_score * 100, 2)
-        
     mid_100.columns = ['ID', 'MID2', 'MID3', 'MID4', 'MID5', 'MID6']
     return mid_100
 
-def merge_mid_final(mid_100,final_100):
+
+def merge_mid_final(mid_100, final_100):
     """
     Merge midterm grades and final grades
-    
+
     Parameters
     ----------
     mid_100: midterm grades dataframe
@@ -110,96 +107,95 @@ def merge_mid_final(mid_100,final_100):
     ----------
     A dataframe containning all grades
     """
-    # merge table
+    # Error meassages
+    if not isinstance(mid_100, pd.DataFrame) is True:
+        raise ValueError("'mid_100' should be should be a panda dataframe.")
+    if not isinstance(final_100, pd.DataFrame) is True:
+        raise ValueError("'final_100' should be should be a panda dataframe.")
+    # Merge two dataframes into one
     grades = mid_100.merge(final_100, how='inner', on='ID')
-    #grades3['FIN1'] = grades3['FIN1'].fillna(0)
     grades.replace(np.nan, 0)
     return grades
 
+
 def standardize_grades(grades):
     """
-    Normalize grades 
-    
+    Normalize grades
+
     Parameters
     ----------
-    A dataframe containning all grades
+    grades: A dataframe containning all grades
 
     Return
     ----------
     A dataframe containning standardized grades
-    
     """
-    cols = ['MID2', 'MID3', 'MID4', 'MID5', 'MID6', 'FIN2', 'FIN3', 'FIN4','FIN5', 'FIN6']
-    ID_var = grades['ID']
+    # Error meassage
+    if not isinstance(grades, pd.DataFrame) is True:
+        raise ValueError("'grades' should be should be a panda dataframe.")
+    # Standzrdize all scores
+    cols = ['MID2', 'MID3', 'MID4', 'MID5', 'MID6', 'FIN2', 'FIN3', 'FIN4', 'FIN5', 'FIN6']
+    id_var = grades['ID']
+    std = StandardScaler()
     features = grades[cols]
-    data_std = StandardScaler().fit_transform(features)
-    data_std = pd.DataFrame(data_std, 
-                        index=features.index,
-                        columns=features.columns)
-    data_std['ID'] = ID_var
+    data_std = std.fit_transform(features)
+    data_std = pd.DataFrame(data_std,
+                            index=features.index,
+                            columns=features.columns)
+    data_std['ID'] = id_var
     return data_std
 
-def get_result(grades3):
+
+def get_result(grades):
     """
     Measure the performance measure for each sesssion
-    
+
     Parameters
     ----------
-    A dataframe containning all standardized grades
+    grades: A dataframe containning all standardized grades
 
     Return
     ----------
     A dataframe with performance measurement
-    
-
     """
-    for i in range(2,7):
+    # Error meassage
+    if not isinstance(grades, pd.DataFrame) is True:
+        raise ValueError("'grades' should be should be a panda dataframe.")
+    # Student behavior changes
+    for i in range(2, 7):
         mid_col = 'MID' + str(i)
         fin_col = 'FIN' + str(i)
         col_name = 'RES' + str(i)
         if i == 2:
-            filter = grades3[fin_col]<(grades3[mid_col]*1.3)
+            behavior_filter = grades[fin_col] < (grades[mid_col]*1.3)
         elif i == 3:
-            filter = grades3[fin_col]<(grades3[mid_col]*1.2)
+            behavior_filter = grades[fin_col] < (grades[mid_col]*1.2)
         elif i == 4:
-            filter = grades3[fin_col]<(grades3[mid_col]*2)
+            behavior_filter = grades[fin_col] < (grades[mid_col]*2)
         elif i == 5:
-            filter = grades3[fin_col]<(grades3[mid_col]*1.8)
+            behavior_filter = grades[fin_col] < (grades[mid_col]*1.8)
         elif i == 6:
-            filter = grades3[fin_col]<(grades3[mid_col]*1.5)
-        grades3[col_name] = filter*1
-    return grades3
-    
+            behavior_filter = grades[fin_col] < (grades[mid_col]*1.5)
+        grades[col_name] = behavior_filter*1
+    return grades
 
-def save_grades(grades,outdir = 'Explore/data/grades2.csv'):
+
+def save_grades(grades, outdir='EPM_dataset/Data/complete_grades.csv'):
     """
-    Save processed grades to the path 
-    
+    Save processed grades to the path
+
     Parameters
     ----------
     grades: well-done grades dataframe to save
     outdir: the path to save files in
 
+    return
+    ----------
+    Show the result of saving grades, "Saved"
     """
+    # Error meassage
+    if not isinstance(grades, pd.DataFrame) is True:
+        raise ValueError("'grades' should be should be a panda dataframe.")
+    # Save
     grades.to_csv(outdir)
-
-def main():
-    mid,fin1,fin2 = read_grades()
-    fin = final_manipulation(fin1,fin2)
-    mid = rebase_mid(mid)
-    grades = merge_mid_final(mid,fin)
-    std_grades = standardize_grades(grades)
-    get_result(std_grades)
-    #save_grades(std_grades)
-    print('Done!')
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
+    return print("Saved")
